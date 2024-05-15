e0fc51a01647f534f016abd8d14052d2fdcd0a1e
unknown <moriawe22@gmail.com>
2024-05-15 10:00:38 +0200

    Implement OfflineFirstRunRepository and RunOverviewViewModel
    - Implemented OfflineFirstRunRepository with methods for getting, upserting, and deleting runs, as well as syncing pending runs.
    - Created RunOverviewViewModel with state management and action handling.
    - Integrated RunRepository in RunOverviewViewModel.
    - These changes improve the data handling and user interface of the run overview screen.

--- core/database/src/main/java/com/moriawe/core/database/RunDatabase.kt
+++ core/database/src/main/java/com/moriawe/core/database/RunDatabase.kt
@@ -3,14 +3,21 @@ package com.moriawe.core.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.moriawe.core.database.dao.RunDao
+import com.moriawe.core.database.dao.RunPendingSyncDao
+import com.moriawe.core.database.entity.DeletedRunSyncEntity
 import com.moriawe.core.database.entity.RunEntity
+import com.moriawe.core.database.entity.RunPendingSyncEntity
 
 @Database(
-    entities = [RunEntity::class],
+    entities = [
+        RunEntity::class,
+        RunPendingSyncEntity::class,
+        DeletedRunSyncEntity::class
+               ],
     version = 1
 )
 abstract class RunDatabase: RoomDatabase() {
 
     abstract val runDao: RunDao
-
+    abstract val runPendingSyncDao: RunPendingSyncDao
 }
